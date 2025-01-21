e8b65f6c5ff757c23893853758c96ee08fed9d90
therealmarkhanna <mark@thetechie.dev>
2025-01-21 20:40:12 +0100

    Asteroids init pygame

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
