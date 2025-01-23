19bfe16636402194fd13ebef2cf6122e328721f5
therealmarkhanna <mark@thetechie.dev>
2025-01-24 00:28:57 +0100

    FPS set to 60

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 pygame.init()
-pygame.time.Clock()
+clock = pygame.time.Clock()
+dt = 0
 from constants import *
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
@@ -15,6 +16,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
