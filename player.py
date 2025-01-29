6c20e1f73262a3c5789eb77d8e9279b74c0d279a
therealmarkhanna <mark@thetechie.dev>
2025-01-29 16:13:53 +0100

    triangle method added to player class

--- player.py
+++ player.py
@@ -1,4 +1,15 @@
 from circleshape import *
 
 class Player(CircleShape):
-    pass
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.rotation = 0
+
+    # in the player class
+def triangle(self):
+    forward = pygame.Vector2(0, 1).rotate(self.rotation)
+    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+    a = self.position + forward * self.radius
+    b = self.position - forward * self.radius - right
+    c = self.position - forward * self.radius + right
+    return [a, b, c]
