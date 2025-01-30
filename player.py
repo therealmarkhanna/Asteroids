75f256876b0c7e319f01cb164ca18207db56aaa4
therealmarkhanna <mark@thetechie.dev>
2025-01-31 00:07:42 +0100

    fixed indention error in player.py

--- player.py
+++ player.py
@@ -1,19 +1,21 @@
 from circleshape import *
+from constants import *
+import pygame
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.rotation = 0
 
-    # in the player class
-def triangle(self):
-    forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    a = self.position + forward * self.radius
-    b = self.position - forward * self.radius - right
-    c = self.position - forward * self.radius + right
-    return [a, b, c]
+        # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
 
-def draw(self, screen):
-        # sub-classes must override
+    def draw(self, screen):
+            # sub-classes must override
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
